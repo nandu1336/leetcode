9733ec5f1c45b18f82fa876406976e2fbd09a9c1
nandakishore <nk732100@gmail.com>
2024-02-18 14:10:54 +0530

    Update 45_min_jumps.py

--- greedy/45_min_jumps.py
+++ greedy/45_min_jumps.py
@@ -14,6 +14,21 @@ def min_jumps(nums: List[int]) -> int:
             min_jumps_needed[index] = 1 + min(min_jumps_needed[index: possible_dest + 1])
     return min_jumps_needed[0]
 
+def min_jumps_2(nums: List[int]) -> int:
+
+    l, r = 0, 0
+    res = 0
+
+    while r < len(nums) - 1:
+        farthest = 0
+        for i in range(l, r + 1):
+            temp = i + nums[i]
+            farthest = farthest if farthest > temp else temp
+        
+        res += 1
+        l, r = r + 1, farthest
+    return res
+
 if __name__ == "__main__":
-    print(min_jumps([2,3,1,1,4]))
-    print(min_jumps([1, 2, 3]))
+    print(min_jumps_2([2,3,1,1,4]))
+    print(min_jumps_2([1, 2, 3]))
