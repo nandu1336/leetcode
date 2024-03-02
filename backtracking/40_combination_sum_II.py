06d8aed6f3d0e492d2c5d8fc0498368c62ed0ba0
nandakishore <nk732100@gmail.com>
2024-03-02 21:11:54 +0530

    Update 40_combination_sum_II.py

--- backtracking/40_combination_sum_II.py
+++ backtracking/40_combination_sum_II.py
@@ -24,5 +24,24 @@ def combination_sum(candidates: List[int], target: int) -> List[List[int]]:
     dfs()
     return res
 
+def combination_sum_2(candidates: List[int], target: int) -> List[List[int]]:
+    candidates.sort()
+    res = []
+    N = len(candidates)
+
+    def dfs(i=0, rem=target, combo=[]):
+        if rem == 0:
+            res.append(combo[:])
+            return 
+        
+        for j in range(i, N):
+            
+            if j > i and candidates[j] == candidates[j-1]: continue
+            if candidates[j] > rem: return 
+        
+            dfs(j+1, rem - candidates[j], combo + candidates[j: j+1])
+    dfs()
+    return res
+
 if __name__ == "__main__":
-    print(combination_sum(candidates=[10,1,2,7,6,1,5], target=8))
+    print(combination_sum_2(candidates=[10,1,2,7,6,1,5], target=8))
