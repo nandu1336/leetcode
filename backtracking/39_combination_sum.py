fdd4c0142ca34668964c6f6e9b53753d4d4a481e
nandakishore <nk732100@gmail.com>
2024-03-02 11:54:27 +0530

    Update 39_combination_sum.py

--- backtracking/39_combination_sum.py
+++ backtracking/39_combination_sum.py
@@ -1,13 +1,13 @@
 from typing import List 
 
-def combination_sum(candidates: List[int], sum: int) -> List[List[int]]:
+def combination_sum(candidates: List[int], target: int) -> List[List[int]]:
     res = []
     N = len(candidates)
 
     def dfs(index, temp_sum, combo):
-        if temp_sum > sum: return 
+        if temp_sum > target: return 
 
-        if temp_sum == sum: 
+        if temp_sum == target: 
             res.append(combo[:])
             return 
         
@@ -23,4 +23,4 @@ def combination_sum(candidates: List[int], sum: int) -> List[List[int]]:
 
     return res
 if __name__ == "__main__":
-    print(combination_sum(candidates=[2, 3, 6, 7], sum=7))
+    print(combination_sum(candidates=[2, 3, 6, 7], target=7))
