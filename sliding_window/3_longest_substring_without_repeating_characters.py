d5f2f91cc20eba2c6a5f26648076ca50050fbdf7
nandakishore <nk732100@gmail.com>
2024-03-13 23:55:49 +0530

    Update 3_longest_substring_without_repeating_characters.py

--- sliding_window/3_longest_substring_without_repeating_characters.py
+++ sliding_window/3_longest_substring_without_repeating_characters.py
@@ -15,6 +15,22 @@ def length_of_longest_substring(s: str) -> int:
             i += 1
     return maxlen
 
+def length_of_longest_substring_2(s: str) -> int:
+    visited = set()
+    l = 0
+    maxlen = 0
+
+    for r in range(len(s)):
+        while s[r] in visited:
+            visited.remove(s[l])
+            l += 1
+        
+        visited.add(s[r])
+        length = r - l + 1
+        maxlen = maxlen if maxlen > length else length
+    
+    return maxlen
+
 if __name__ == "__main__":
     # print(length_of_longest_substring(s="abcabcbb"))
-    print(length_of_longest_substring(s="bbbbb"))
+    print(length_of_longest_substring_2(s="bbbbb"))
